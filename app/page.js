e6ee30d1be580c01af69d14db99475328b2bfe31
thanhdanhhnad4-spec <thanhdanhhnad4@gmail.com>
2026-04-30 12:10:49 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 100 }}>
+      <h1>Locket Viewer</h1>
+      <p>Try /yourname</p>
+    </div>
+  );
+}
